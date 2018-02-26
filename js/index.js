14822c6e835649ee5975522e7abee9e406725f66
Tech-A47 <tech-a47@Tech-A47s-MacBook-Air.local>
2018-02-26 16:08:51 +0000

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,29 @@
+$(function(){
+
+	//alert("Yo");
+	var move1 = false;
+	var move2 = false;
+	var tiles = $(".peg");
+
+	
+
+	tiles.on("click", function(event){
+		if(move1 === false){
+			move1 = $(this);
+			console.log(move1.attr("data-num"));
+		} else if(move1 !== false){
+			move2 = $(this);
+			console.log(move2.attr("data-num"));
+			//console.log(move2.attr("#box5"));
+/*			move1 = $(move1).html("");
+			move2 = $(move2).html("");*/
+			move1 = false;
+			move2 = false;	
+		}
+	});
+	}
+);
+
+var id = parseInt(this.id); //19
+    --id; 
+var previousbox = $("#box" + id); //box18
