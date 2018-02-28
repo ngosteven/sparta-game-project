8c5f754049d91944c7f3cecca0c336168d573db6
Tech-A47 <tech-a47@192.168.1.110>
2018-02-28 23:48:34 +0000

    end game and css styling

--- js/index.js
+++ js/index.js
@@ -1,71 +1,129 @@
 $(function(){
-	var move1 = false;
-	var move2 = false;
+	var hopped;
 	var move1sum;
 	var move2sum;
+	var move1 = false;
+	var move2 = false;
 	var value = false;
+	var moves = 0;
+	var totalSeconds = 0;
+	var moved = $("#place");
 	var tiles = $(".peg");
-	var hopped;
-	var counter = 0;
-		tiles.on("click", function(event){
-			if(move1 == false){
-				move1 = $(this);
-				move1sum = (move1.attr("data-num"));
-				value = $(this).html();
-				console.log(move1sum);	
-				if(value == "_"){
-					alert("invalid move1");
-					move1 = false;
-				}
-			} else if(move1 !== false){
-				move2 = $(this);
-				move2sum = (move2.attr("data-num"));
-				value = $(this).html();
-				console.log(move2sum);
-				if(value == "O"){
-					alert("invalid move2");
-					move1 = false;
-					move2 = false;
-				} else if(Math.abs(move1sum-move2sum) ==2){
-					hopped = (parseInt(move1sum)+parseInt(move2sum))/2;
-					var hoppedBox = ("#box"+hopped);
-					if($(hoppedBox).html() == "_"){
-						alert("Invalid movehop");
-						move1 = false;
-						move2 = false;
-					} else {
-						$(hoppedBox).html("_");
-						$(move1).html("_");
-						$(move2).html("O");
-						move1 = false;
-						move2 = false;
-						//console.log(move1 + " " + move2);
-						counter = counter + 1;
-						console.log(counter);
-					}
-				} else if(Math.abs(move1sum-move2sum) ==14){
-					hopped = (parseInt(move1sum)+parseInt(move2sum))/2;
-					var hoppedBox = ("#box"+hopped);
-					if($(hoppedBox).html() == "_"){
-						alert("Invalid movehop");
-						move1 = false;
-						move2 = false;
-					} else {
-						$(hoppedBox).html("_");
-						$(move1).html("_");
-						$(move2).html("O");
-						move1 = false;
-						move2 = false;
-						//console.log(move1 + " " + move2);
-						counter = counter + 1;
-						console.log(counter);
-					}
-				} else {
-					alert("Invalid move");
-					move1 = false;
-					move2 = false;
-				}
+	var finish = $(".buttonEnd");
+	var clear = $(".buttonClear");
+	var minutesLabel = document.getElementById("minutes");
+	var secondsLabel = document.getElementById("seconds");
+	var audio = new Audio('jazz.mp3');
+	
+	audio.play();
+
+	finish.on("click", function(event){
+		var ballCount = 0;
+		for (var i = 0; i < tiles.length; i++) {
+			if($(tiles[i]).html() == "O"){
+				ballCount++;
 			}
-		});
+		}
+		alert("You finished with " + ballCount + " left on the board");
+		location.reload();
+	});
+
+	setInterval(setTime, 1000);
+	function setTime() {
+	  	++totalSeconds;
+	 	secondsLabel.innerHTML = pad(totalSeconds % 60);
+	  	minutesLabel.innerHTML = pad(parseInt(totalSeconds / 60));
 	}
-);
+
+	function pad(val) {
+		var valString = val + "";
+		if (valString.length < 2) {
+		    return "0" + valString;
+		} else {
+		    return valString;
+		}
+	}
+
+	tiles.on("click", function(event){
+
+		if(move1 == false){
+			move1 = $(this);
+			move1sum = (move1.attr("data-num"));
+			value = $(this).html();
+			$(this).css('background','darkred');
+			if(value == "_"){
+				alert("invalid move1");
+				$(move1).css('background','');
+				move1 = false;
+			}
+
+		} else if(move1 !== false){
+			move2 = $(this);
+			move2sum = (move2.attr("data-num"));
+			value = $(this).html();
+
+			if(value == "O"){
+				alert("invalid move2");
+				$(move1).css('background','');
+				move1 = false;
+				move2 = false;
+
+			} else if(Math.abs(move1sum-move2sum) ==2){
+				hopped = (parseInt(move1sum)+parseInt(move2sum))/2;
+				var hoppedBox = ("#box"+hopped);
+				$(move1).css('background','');
+
+				if($(hoppedBox).html() == "_"){
+					alert("Invalid movehop");
+					$(move1).css('background','');
+					move1 = false;
+					move2 = false;
+
+				} else {
+					$(hoppedBox).html("_");
+					$(move1).html("_");
+					$(move2).html("O");
+					move1 = false;
+					move2 = false;
+					moves++;
+					$(moved).html(moves);
+
+					if (moves >= 31){
+						alert("You beat the board!");
+					}					
+				}
+
+			} else if(Math.abs(move1sum-move2sum) ==14){
+				hopped = (parseInt(move1sum)+parseInt(move2sum))/2;
+				var hoppedBox = ("#box"+hopped);
+				$(move1).css('background','');
+
+				if($(hoppedBox).html() == "_"){
+					alert("Invalid movehop");
+					$(move1).css('background','');
+					move1 = false;
+					move2 = false;
+
+				} else {
+					$(hoppedBox).html("_");
+					$(move1).html("_");
+					$(move2).html("O");
+					move1 = false;
+					move2 = false;
+					moves++;
+					$(moved).html(moves);
+
+					if (moves >= 31){
+						alert("You beat the board!");
+					}
+				}
+
+			} else {
+				alert("Invalid move");
+				$(move1).css('background','');
+				move1 = false;
+				move2 = false;
+			}
+		}
+	});
+});
